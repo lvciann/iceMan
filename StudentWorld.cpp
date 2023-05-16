b8cca509338bf335de90a6c866df83e72fe246b6
Luciann Valencia <luciannvalencia@gmail.com>
2023-05-15 23:46:43 -0700

    added StudentWorld

---
+++ StudentWorld.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Luciann Valencia on 5/15/23.
+//
+
+#include "StudentWorld.h"
---
+++ StudentWorld.h
@@ -0,0 +1,14 @@
+//
+// Created by Luciann Valencia on 5/15/23.
+//
+
+#ifndef ICEMAN_STUDENTWORLD_H
+#define ICEMAN_STUDENTWORLD_H
+
+
+class StudentWorld {
+
+};
+
+
+#endif //ICEMAN_STUDENTWORLD_H
