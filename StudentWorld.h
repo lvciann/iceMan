da1016dffe737783c24092f7876cd913b5d16d47
Luciann Valencia <luciannvalencia@gmail.com>
2023-05-23 19:23:01 -0700

    updated StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -8,7 +8,7 @@
 #include "GameConstants.h"
 #include "Actor.h"
 
-//we want create a constant to be able to use 60 and 64 continuously
+//we want to create a constant to be able to use 60 and 64 continuously
 const int ICE_WIDTH = 64;
 const int ICE_HEIGHT = 60;
 
@@ -63,14 +63,32 @@ public:
     //
     virtual int move()
     {
+        //part 1: JUST THE ICEMAN--during each tick, must ask Iceman object to do something;
+        if (iceman != nullptr){   //if iceman is alive
+            //ask iceman to do something
+            iceman->doSomething();
+        }
+
         // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
         // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
         decLives();
         return GWSTATUS_PLAYER_DIED;
     }
 
+    //page 10
     virtual void cleanUp()
     {
+        if(iceman != nullptr){
+            delete iceman;
+        }
+
+        for (int i = 0; i < ICE_HEIGHT; ++i) { //for rows
+            for (int j = 0; j < ICE_WIDTH; ++j){ //for columns
+                if(ice[i][j] != nullptr) {
+                    delete ice[i][j];
+                }
+            }
+        }
     }
 };
 
