2cdab7d5342c2144e3cb5a4639d4ad55c40c9237
lvciann <105315522+lvciann@users.noreply.github.com>
2023-05-27 22:21:29 -0700

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -6,16 +6,84 @@
 #include <string>
 using namespace std;
 
-StudentWorld* theWorld = nullptr;
 
 GameWorld* createStudentWorld(string assetDir)
 {
-    theWorld = new StudentWorld(assetDir);
-    return theWorld;
+    return new StudentWorld(assetDir);
 }
 
-StudentWorld* getWorld() {
-    return theWorld;
+StudentWorld::StudentWorld(std::string assetDir) : GameWorld(assetDir),
+iceman(nullptr) {
+    for (int i = 0; i < ICE_WIDTH; ++i) { //for rows
+        for (int j = 0; j < ICE_HEIGHT; ++j) { //for columns
+            ice[i][j] = nullptr;
+        }
+    }
+
 }
 
-// Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
+StudentWorld::~StudentWorld() {
+    //if iceman doesn't find ice, delete ice
+    if (iceman != nullptr) {
+        delete iceman;
+    }
+    for (int i = 0; i < ICE_WIDTH; ++i) { //for rows
+        for (int j = 0; j < ICE_HEIGHT; ++j) { //for columns
+            if (ice[i][j] != nullptr) {
+                delete ice[i][j];
+            }
+        }
+    }
+}
+
+//initialize the data structures used to keep track of game's virtual world
+//construct new oil field
+//allocate and insert a valid Iceman object at the proper location
+int StudentWorld::init()
+{
+    this->iceman = new Iceman(this, 30, 60);
+
+    for (int i = 0; i < ICE_HEIGHT; ++i) { //for rows
+        for (int j = 0; j < ICE_WIDTH; ++j) { //for columns
+            if (j < 30 || j > 34 || (j >= 30 && j <= 34 && i < 4)) {
+                ice[i][j] = new Ice(this, i, j);
+            }
+        }
+    }
+
+    return GWSTATUS_CONTINUE_GAME;
+}
+
+//
+int StudentWorld::move()
+{
+    //part 1: JUST THE ICEMAN--during each tick, must ask Iceman object to do something;
+    if (iceman != nullptr) {   //if iceman is alive
+        //ask iceman to do something
+        iceman->doSomething();
+        return GWSTATUS_CONTINUE_GAME;      // so that the game can continue
+    }
+
+    // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+    // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+
+    decLives();
+    return GWSTATUS_PLAYER_DIED;
+}
+
+//page 10
+void StudentWorld::cleanUp()
+{
+    if (iceman != nullptr) {
+        delete iceman;
+    }
+
+    for (int i = 0; i < ICE_HEIGHT; ++i) { //for rows
+        for (int j = 0; j < ICE_WIDTH; ++j) { //for columns
+            if (ice[i][j] != nullptr) {
+                delete ice[i][j];
+            }
+        }
+    }
+}
+
