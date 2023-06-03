5445779de9590bd2e37a673b99c4349492b1dc78
lvciann <105315522+lvciann@users.noreply.github.com>
2023-06-03 15:27:06 -0700

    Update StudentWorld.cpp

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -26,27 +26,36 @@ iceman(nullptr) {
 int StudentWorld::init()
 {
     this->iceman = new Iceman(this, 30, 60); //position of iceman
+    std::vector<Actor *> actors; 
 
     for (int i = 0; i < ICE_WIDTH; ++i) { //for columns
         for (int j = 0; j < ICE_HEIGHT; ++j) { //for rows
             if (i < 30 || i > 33 || (i >= 30 && i <= 34 && j < 4)) {
                 ice[i][j] = new Ice(this, i, j);
             }
-            //testing for orientation--delete asap
-            if(i == 0 && j == 0){
-                delete ice[0][0];
-            }
-            if(i == 30 && j == 0){
-                delete ice[30][0];
-            }
-        }
-        
-        
-        //distributing game objects
-        //boulderDist();
-        //goldDist();
-    }
 
+        }
+    }
+    //distributing game objects randomly
+    for(int i = 0; i < boulderAmnt(); i++){
+        if(i < (ICE_WIDTH - 30 ) || i > (ICE_WIDTH - 27) || (i >= (ICE_WIDTH - 30) && i <= (ICE_WIDTH - 26) && (ICE_HEIGHT < 4))){
+            actors.push_back(new Boulder(this, boulderRandomX(), boulderRandomY()));
+        }
+    }
+    for(int i = 0; i < goldNuggetAmnt(); i++){
+        if(i < (ICE_WIDTH - 30 ) || i > (ICE_WIDTH - 27) || (i >= (ICE_WIDTH - 30) && i <= (ICE_WIDTH - 26) && (ICE_HEIGHT < 4))){
+            actors.push_back(new GoldNugget(this, goldRandomX(), goldRandomY()));
+        }
+    }
+    for(int i = 0; i < barrelAmnt(); i++){
+        if(i < (ICE_WIDTH - 30 ) || i > (ICE_WIDTH - 27) || (i >= (ICE_WIDTH - 30) && i <= (ICE_WIDTH - 26) && (ICE_HEIGHT < 4))){
+            actors.push_back(new OilBarrel(this, barrelRandomX(), barrelRandomY()));
+        }
+    }
+        
+        
+        
+    
 
     return GWSTATUS_CONTINUE_GAME;
 }
@@ -62,14 +71,15 @@ int StudentWorld::move()
     if(iceman->isActive() == true){ //if iceman is alive
         //ask iceman to do something
         iceman->doSomething();
+        if(iceman->hasCompletedLevel(min((2 + (int) getLevel()), 21))){
+            playSound(GWSTATUS_FINISHED_LEVEL);
+            return GWSTATUS_FINISHED_LEVEL;
+        }
         return GWSTATUS_CONTINUE_GAME;      // so that the game can continue
     }
-    
+
     ///////////////////////////////////////////next level////////////////////////////////////////////////////////
-    if(barrelAmnt() == 0){
-        playSound(GWSTATUS_FINISHED_LEVEL);
-        return GWSTATUS_FINISHED_LEVEL;
-    }
+
     if(goldNuggetAmnt() == 0){
         
     }
@@ -165,32 +175,26 @@ int StudentWorld::barrelAmnt(){
     int L = min(2 + (int) (getLevel()), 21);
     return L;
 }
-void StudentWorld::boulderDist(){
-    for(int i = 0; i < boulderAmnt(); i++){
-        actors.push_back(new Boulder(this, boulderRandomX(), boulderRandomY()));
-    }
-}
-void StudentWorld::goldDist(){
-    for(int i = 0; i < 1; i++){
-        actors.push_back(new GoldNugget(this, goldRandomX(), goldRandomY()));
-    }
-}
+
 //------------------------------------area for actors in oil field------------------------------------
 
 void StudentWorld::clearIce(int x, int y){
     //if iceman's location == the location of ice, delete ice
     //took the x(+- 1) and y(+- 3) position of iceman then made a for loop from the current position to calculated position to delete\\
     
-//    for(int i = x; i < (x + 1); i++){ //for row
-//        for(int j = y; j < (y + 3); j++){ //for column
-//            if(iceman = ice[x][y]){
-//                delete ice[i][j];
-//            }
-//        }
-//
-//    }
 
         delete ice[x][y];
-    
 }
-
+//checking if there is ice here
+bool StudentWorld::isIce(){
+    for(int i = 0; i < ICE_HEIGHT; i++){ //check rows
+        for(int j = 0; j < ICE_WIDTH; j++){ //check column
+        //check if ice is pointing to nothing
+            if(ice[i][j] != 0){
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
